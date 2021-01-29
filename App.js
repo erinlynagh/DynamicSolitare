6744e0b23c8f1da49ff130da9358e914db85b0f8
Eoin <debatebroshow@gmail.com>
2021-01-28 19:21:57 -0500

    some stuff working

--- App.js
+++ App.js
@@ -1,23 +1,86 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';  
-import PlayingCard from './JS/PlayingCard'
+import { StyleSheet, Text, View } from 'react-native';
+import PlayingCard from './JS/PlayingCard';
+import CardDeck from './JS/CardDeck';
+import { Container, Row, Col, Jumbotron, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <PlayingCard id="2C"/>
-    </View>
+    <Container align="center">
+      <Jumbotron>
+        <h1>Cards</h1>
+      </Jumbotron>
+      <Row className='mb-5'>
+        <Col>
+          <PlayingCard id="purple_back" />
+        </Col>
+        <Col>
+          <PlayingCard id="red_back" />
+        </Col>
+        <Col>
+          <PlayingCard id="yellow_back" />
+        </Col>
+        <Col>
+          <PlayingCard id="yellow_back" />
+        </Col>
+        <Col>
+          <PlayingCard id="yellow_back" />
+        </Col>
+        <Col>
+          <PlayingCard id="yellow_back" />
+        </Col>
+        <Col>
+          <PlayingCard id="gray_back" />
+        </Col>
+      </Row>
+      <Row className="mb-2">
+        <Col>
+          <CardDeck id="2"/>
+        </Col>
+        <Col>
+        <CardDeck id="3"/>
+        </Col>
+        <Col>
+          <PlayingCard id="2H" />
+        </Col>
+        <Col>
+          <PlayingCard id="5D" />
+        </Col>
+        <Col>
+          <PlayingCard id="8C" />
+        </Col>
+        <Col>
+          <PlayingCard id="2C" />
+        </Col>
+        <Col>Score1</Col>
+      </Row>
+      <Row className="mb-2">
+      <Col>
+      <CardDeck id="4"/>
+        </Col>
+        <Col>
+        <CardDeck id="5"/>
+        </Col>
+        <Col>
+          <PlayingCard id="2H" />
+        </Col>
+        <Col>
+          <PlayingCard id="5D" />
+        </Col>
+        <Col>
+          <PlayingCard id="8C" />
+        </Col>
+        <Col>
+          <PlayingCard id="2C" />
+        </Col>
+        <Col>Score2</Col>
+      </Row>
+      <Row>
+        <Button className="btn btn-block">Deal</Button>
+      </Row>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- JS/PlayingCard.js
+++ JS/PlayingCard.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import {StyleSheet,Image,Text} from 'react-native';
+import {StyleSheet,Image,View,Dimensions} from 'react-native';
+import {Card} from 'react-bootstrap';
+
+const { width } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-    card: {
-      width: 110,
-      height: 170,
-    },
-  });
+  thumbContainer: {
+    width: width*0.075,
+    height: width*1.52821*0.075,
+  },
+  card: {
+    flex: 1,
+    width: undefined,
+    height: undefined,
+    position: 'relative',
+    resizeMode: 'cover'
+  },
+});
 
 class PlayingCard extends React.Component {
-
     render() {
         var id = this.props.id;
         return([
-            <Image style={styles.card} source={require("../public/" + id + ".png")}/>
+            <View style={styles.thumbContainer}>
+              <Image style={styles.card} source={require("../public/" + id + ".png")}/>
+            </View>
         ]);
-    }
-    
+    } 
   }
 export default PlayingCard;
